7c0f098ffe375117b709c5c0ad702cb054be9315
m-shafafi <m-shafafi@agri-bank.com>
2025-03-09 14:23:37 +0330

    added paramaters to UpdateProductRequest method

--- src/Services/shop/MedicalShop.Contracts/MedicalShop.Contracts/Requests/Medical/UpdateProductRequest.cs
+++ src/Services/shop/MedicalShop.Contracts/MedicalShop.Contracts/Requests/Medical/UpdateProductRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace MedicalShop.Contracts.Requests.Medicals;
 
-public record UpdateProductRequest(string Title, string Description, string Category);
+public record UpdateProductRequest(sint Id, string Name, string Description, decimal Price, int? BrandID, int? CategoryID, string StockQuantity, string SKU, string ImageURL, string Warranty, int Rating);
